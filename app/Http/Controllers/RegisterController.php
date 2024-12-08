feee7d9a9b73f447375ee343d1310c9540ceb04c
Gissel-1230 <gis_kar@hotmail.com>
2024-12-08 16:02:04 -0600

    primercommit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function welcome(Request $request)
+    {
+        //dd($request->get('username'));
+        //Validación
+        $validate=$request->validate([
+            'name'=>'required|min:3',
+            'username'=>'required|unique:users|min:3|max:30',
+            'email'=>'required|email|unique:users|email|max:60',
+            'password'=> 'required'
+        ]);
+
+        
+    }
+}
