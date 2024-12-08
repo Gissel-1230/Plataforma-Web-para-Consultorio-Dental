f02712ee4302d5ba39cef25b29d54e436fc8823e
Gissel-1230 <gis_kar@hotmail.com>
2024-12-08 16:48:00 -0600

    migraciones

--- database/migrations/2024_12_08_224425_add_username_to_users_table.php
+++ database/migrations/2024_12_08_224425_add_username_to_users_table.php
@@ -12,19 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username');
-            //
+            $table->string('username')->unique()->after('email');
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('username');
-            //
         });
     }
+    
 };
